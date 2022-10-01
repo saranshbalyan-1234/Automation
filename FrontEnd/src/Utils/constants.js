9abe8a97dc69dda7b23112e802fd04f51b8aabef
saransh balyan <saransh@kditsolutions.com>
2022-10-01 16:05:11 +0530

    frontend docker

--- FrontEnd/src/Utils/constants.js
+++ FrontEnd/src/Utils/constants.js
@@ -1,2 +1,3 @@
-// export const api_base_url = "https://timetrackerrbackend.herokuapp.com";
-export const api_base_url = "http://localhost:3001";
+export const api_base_url =
+  "https://automationbackend121321313131232131.onrender.com";
+// export const api_base_url = "http://localhost:3001";
