ceb100f1029d7e9b3f0a0ae9acefea0742165415
saransh balyan <saransh@kditsolutions.com>
2022-10-02 03:47:48 +0530

    added app swicther view

--- FrontEnd/src/Components/Layout/Header/ProfileMenu.jsx
+++ FrontEnd/src/Components/Layout/Header/ProfileMenu.jsx
@@ -8,9 +8,11 @@ import {
   BellOutlined,
 } from "@ant-design/icons";
 import { BiSupport } from "react-icons/bi";
-import { Avatar, Dropdown, Menu, Badge } from "antd";
+import { TbApps } from "react-icons/tb";
+import { Avatar, Dropdown, Menu, Badge, Card } from "antd";
 import { logout } from "../../../Redux/Actions/auth";
 import { Link } from "react-router-dom";
+const { Meta } = Card;
 const ProfileMenu = ({ logout }) => {
   const profileMenu = (
     <Menu
@@ -93,6 +95,54 @@ const ProfileMenu = ({ logout }) => {
     />
   );
 
+  const selectAppMenu = (
+    <Menu
+      items={[
+        {
+          label: (
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+              <Card hoverable style={{ width: 200 }}>
+                <Meta
+                  title="Automation"
+                  description="Test your Product and Execute TestCases."
+                />
+              </Card>
+              <Card hoverable style={{ width: 200 }}>
+                <Meta
+                  title="Time Tracker"
+                  description="Track your employee productivity."
+                />
+              </Card>
+            </div>
+          ),
+          key: "1",
+          // onClick: logout,
+        },
+
+        {
+          label: (
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+              <Card hoverable style={{ width: 200 }}>
+                <Meta
+                  title="Swagger"
+                  description="Create your Api Documentation."
+                />
+              </Card>
+              <Card hoverable style={{ width: 200 }}>
+                <Meta
+                  title="No Code Solution"
+                  description="Create Api without coding."
+                />
+              </Card>
+            </div>
+          ),
+          key: "2",
+          // onClick: logout,
+        },
+      ]}
+    />
+  );
+
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
       <div
@@ -102,6 +152,11 @@ const ProfileMenu = ({ logout }) => {
           cursor: "pointer",
         }}
       >
+        <Dropdown overlay={selectAppMenu} trigger={["hover"]}>
+          <TbApps
+            style={{ color: "white", fontSize: "20px", marginRight: "10px" }}
+          />
+        </Dropdown>
         <Dropdown overlay={notificationMenu} trigger={["hover"]}>
           <Badge count={1} overflowCount={9}>
             <BellOutlined style={{ color: "white", fontSize: "20px" }} />
