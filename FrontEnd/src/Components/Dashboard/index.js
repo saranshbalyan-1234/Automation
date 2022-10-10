9f8ef03a9ac2a2e8c96e292f27886df545f78c20
saransh balyan <saransh@kditsolutions.com>
2022-10-11 00:13:58 +0530

    dashboard

--- FrontEnd/src/Components/Dashboard/index.js
+++ FrontEnd/src/Components/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Typography } from "antd";
+import { Typography, Statistic, Row, Col, Card } from "antd";
 // import axios from "axios";
 
 import Tree from "../Common/Tree";
@@ -10,10 +10,31 @@ export const Dashboard = ({ user }) => {
   }, []);
 
   return (
-    <div>
-      <Typography.Title>Hi, {user.name}</Typography.Title>
-      {/* <Search /> */}
-      <Tree />
+    <div style={{ paddingTop: 20 }}>
+      <Typography.Title level={3}>Hi, {user.name}</Typography.Title>
+      <Row gutter={[16, 16]}>
+        <Col>
+          <Card style={{ backgroundColor: "#f6f6f6" }}>
+            <Statistic title="Test Cases" value={2} />
+          </Card>
+        </Col>
+
+        <Col>
+          <Card style={{ backgroundColor: "#f6f6f6" }}>
+            <Statistic title="Reusable Flows" value={29} />
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ backgroundColor: "#f6f6f6" }}>
+            <Statistic title="Object Bank" value={15} />
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ backgroundColor: "#f6f6f6" }}>
+            <Statistic title="Team Members" value={55} />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
