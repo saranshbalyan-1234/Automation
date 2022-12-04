8f386f42d1b80a36b4144f60efed5f33f89da2a5
saransh balyan <saransh@kditsolutions.com>
2022-12-04 22:36:39 +0530

    done everything

--- Backend/Models/Global/PermissionList.js
+++ Backend/Models/Global/PermissionList.js
@@ -1,14 +1,18 @@
 export default (sequelize, DataTypes) => {
-  const PermissionList = sequelize.define("permissionList", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: "name",
-      validate: {
-        notNull: true,
+  const PermissionList = sequelize.define(
+    "permissionList",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: "name",
+        validate: {
+          notNull: true,
+        },
       },
     },
-  });
+    { timestamps: false }
+  );
 
   return PermissionList;
 };
